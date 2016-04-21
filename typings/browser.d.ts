299b80c3b407c52aac08ec622f4fcca1a09632b0
xmilygit <xmilygame@163.com>
2016-04-22 01:44:50 +0800

    first modi

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,1 +1,3 @@
+/// <reference path="browser/ambient/express-serve-static-core/index.d.ts" />
+/// <reference path="browser/ambient/express/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,1 +1,3 @@
+/// <reference path="main/ambient/express-serve-static-core/index.d.ts" />
+/// <reference path="main/ambient/express/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
