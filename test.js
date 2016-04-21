25ff62b824215b3e7da88d394603bdc037214d3f
xmilygit <xmilygame@163.com>
2016-04-21 11:46:44 +0800

    program start

---
+++ test.js
@@ -0,0 +1,35 @@
+var http = require('http'),
+    fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('500-Internal Error');
+        } else {
+            res.writeHead(responseCode, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    })
+}
+
+http.createServer(function (req, res) {
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch (path) {
+        case '':
+            serveStaticFile(res, '/public/home.html', 'text/html');
+            break;
+        case '/about':
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo.jpg':
+            serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
+            break;
+        default:
+            serveStaticFile(res, '/public/404.html', 'text/html', 404);
+            break;
+    }
+}).listen(3000);
+
+console.log('服务器已经开始运行，地址：http://localhost:3000  结束服务请按 ctrl+c 键');
