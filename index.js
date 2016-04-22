ee802267f7d3dd2a1371aea59146fbc9c17a33cd
xmilygit <xmilygame@163.com>
2016-04-22 13:15:35 +0800

    添加handlebars视图引擎

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-var express=require('express');
-var app=express();
+var express = require('express');
+var handlebars = require('handlebars');
 
-app.set('port',process.env.PORT||3000);
+var app = express();
 
-app.get('/',function (req,res) {
-    res.type('text/plain');
-    res.send('首页');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+    //res.type('text/plain');
+    //res.send('首页');
+    res.render('home');
 });
 
-app.get('/about',function (req,res) {
-    res.type('text/plain');
-    res.send('about this web');
+app.get('/about', function (req, res) {
+    //res.type('text/plain');
+    //res.send('about this web');
+    res.render('about');
 })
 
 
 //定制404页面
-app.use(function (req,res) {
-    res.type('text/plain');
+app.use(function (req, res, next) {
+    //res.type('text/plain');
     res.status(404);
-    res.send('404 Not Found');
+    res.render('404');
 })
 
 //定制500页面
-app.use(function (err,req,res,next) {
+app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
+    //res.type('text/plain');
     res.status(500);
-    res.send('500 Server Error');
+    res.render('500');
 });
 
-app.listen(app.get('port'),function () {
-    console.log('Express服务已经启动 地址:http://localhost:'+app.get('port')+'; 按ctrl+c键关闭服务器');
+app.listen(app.get('port'), function () {
+    console.log('Express服务已经启动 地址:http://localhost:' + app.get('port') + '; 按ctrl+c键关闭服务器');
 })
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="main/ambient/express-serve-static-core/index.d.ts" />
 /// <reference path="main/ambient/express/index.d.ts" />
+/// <reference path="main/ambient/handlebars/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
